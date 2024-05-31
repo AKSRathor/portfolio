c21f91926f326e26a39ddaed2e6f5b0fcee458e9
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2024-05-31 10:15:13 +0530

    new items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeLand from './Components/HomeLand';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
+      <Analytics/>
       <HomeLand/>
     </div>
   );
