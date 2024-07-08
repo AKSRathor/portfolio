f5e623a7558b6aac4734b9b56fedfc1e579fa924
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2024-07-08 23:44:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import HomeLand from './Components/HomeLand';
 import { Analytics } from "@vercel/analytics/react"
 import Chatbot from './Components/Chatbot';
 
+const myfunc = async()=> {
+  const response = await fetch(`https://llm-verifier.vercel.app/selectmodel`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ })
+  })
+  await response.json()
+
+}
+myfunc()
+
 function App() {
   return (
     <div className="App">
